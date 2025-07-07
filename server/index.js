5181c8f58e0245af7e60c807c0b4b73969878fa0
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-07 12:01:48 +0200

    Set up basic express server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRoutes from "./routes/post/post.routes.js"
+import authRoutes from "./routes/auth/auth.routes.js"
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/posts", postRoutes);
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
