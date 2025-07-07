9c31ae0483ce7e704b21424804b05156211e8091
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-07 15:48:29 +0200

    Create JWT file structure and utility

---
+++ server/utils/jwt.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export function generateToken(user) {
+  return jwt.sign(
+    { id: user.id, username: user.username, role: user.role },
+    JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+}
+
+export function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
