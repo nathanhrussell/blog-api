6ee9d1712ad9bbe7057af9cef1129991a341b244
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-07 12:07:44 +0200

    Switch to ESM

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import postRoutes from "./routes/post/post.routes.js"
-import authRoutes from "./routes/auth/auth.routes.js"
+import postRoutes from "./routes/post/post.routes.js";
+import authRoutes from "./routes/auth/auth.routes.js";
 
 dotenv.config();
 
@@ -16,9 +16,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-    res.send("API is running...");
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
