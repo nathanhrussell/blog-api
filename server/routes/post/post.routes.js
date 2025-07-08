6fe78b3d032c6d5c92c42cfd893402fd0bce998e
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-08 10:35:57 +0200

    Define routes

--- server/routes/post/post.routes.js
+++ server/routes/post/post.routes.js
@@ -1,8 +1,23 @@
 import express from "express";
+import {
+    getAllPublished,
+    getPublishedById,
+    createPost,
+    updatePost,
+    deletePost,
+    togglePublish
+} from "../../controllers/post/post.controller.js";
+
+import { authenticateUser, requireAuthorRole } from "../../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Get all posts");
-});
+router.get("/", getAllPublished);
+router.get("/:id", getPublishedById);
+
+router.post("/", authenticateUser, requireAuthorRole, createPost);
+router.put("/:id", authenticateUser, requireAuthorRole, updatePost);
+router.delete("/:id", authenticateUser, requireAuthorRole, deletePost);
+router.patch("/:id/publish", authenticateUser, requireAuthorRole, togglePublish);
 
 export default router;
