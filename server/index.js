2674402d1ca53c091fdc0dff9705233a37a78fdd
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-08 10:59:05 +0200

    Add comment route to index.js

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/post/post.routes.js";
 import authRoutes from "./routes/auth/auth.routes.js";
+import commentRoutes from "./routes/comment/comment.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api", commentRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
