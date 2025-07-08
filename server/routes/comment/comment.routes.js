040d7f5d1bb9392b990bbaf6fa27ca3b6caecc95
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-08 10:58:08 +0200

    Implement comment routes

---
+++ server/routes/comment/comment.routes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+    getCommentsByPost,
+    createComment,
+    deleteComment,
+    updateComment
+} from "../../controllers/comment/comment.controller.js";
+
+import { authenticateUser, requireAuthorRole } from "../../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/posts/:id/comments", getCommentsByPost);
+router.post("/posts/:id/comments", createComment);
+
+router.delete("/comments/:id", authenticateUser, requireAuthorRole, deleteComment);
+router.put("/comments/:id", authenticateUser, requireAuthorRole, updateComment);
+
+export default router;
