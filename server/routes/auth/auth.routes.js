f9948956ab9a59ce8a63ea2e169022a4654001cc
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-09 10:36:40 +0200

    Add validation middleware and import

--- server/routes/auth/auth.routes.js
+++ server/routes/auth/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
 import { register, login } from "../../controllers/auth/auth.controller.js"
+import { requireFields } from "../../../middlewares/validate.js";
 
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login)
+
+router.post("/register", requireFields(["username", "password"]), register);
 
 export default router;
