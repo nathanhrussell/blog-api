161e95bb0c2a324746813e17fcedbacbcbaaf708
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-10 09:29:43 +0200

    Set up client-author with vite and tailwind

---
+++ client-author/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-3xl font-bold">Author Portal</h1>
+      <p className="text-gray-600">Login to manage posts.</p>
+    </div>
+  );
+}
+
+export default App;
