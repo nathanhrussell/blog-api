6da3256704a8728e143aa8788325ada9a31c8be4
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-14 11:42:58 +0200

    Ensure post routes are correctly prefixed

--- server/routes/post/post.routes.js
+++ server/routes/post/post.routes.js
@@ -1,32 +1,29 @@
 import express from "express";
 import {
-    getAllPublished,
-    getPublishedById,
-    createPost,
-    updatePost,
-    deletePost,
-    togglePublish,
-    getAllByAuthor,
-    getPostById
+  getAllPublished,
+  getPublishedById,
+  createPost,
+  updatePost,
+  deletePost,
+  togglePublish,
+  getAllByAuthor,
+  getPostById
 } from "../../controllers/post/post.controller.js";
 
 import { getPostsWithCommentsByAuthor } from "../../controllers/comment/comment.controller.js";
-
 import { authenticateUser, requireAuthorRole } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/posts", authenticateUser, getAllByAuthor);
 router.get("/posts/:id", authenticateUser, getPostById);
+router.get("/posts-with-comments", authenticateUser, getPostsWithCommentsByAuthor);
+router.post("/posts", authenticateUser, requireAuthorRole, createPost);
+router.put("/posts/:id", authenticateUser, requireAuthorRole, updatePost);
+router.delete("/posts/:id", authenticateUser, requireAuthorRole, deletePost);
+router.patch("/posts/:id/publish", authenticateUser, requireAuthorRole, togglePublish);
 
 router.get("/", getAllPublished);
 router.get("/:id", getPublishedById);
-
-router.post("/", authenticateUser, requireAuthorRole, createPost);
-router.put("/:id", authenticateUser, requireAuthorRole, updatePost);
-router.delete("/:id", authenticateUser, requireAuthorRole, deletePost);
-router.patch("/:id/publish", authenticateUser, requireAuthorRole, togglePublish);
-
-router.get("/posts-with-comments", authenticateUser, getPostsWithCommentsByAuthor);
 
 export default router;
