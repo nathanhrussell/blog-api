e7e22fb77fbc940b86376488909ffb18da77fc7b
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-07-14 13:03:29 +0200

    Change element in App.jsx to ensure posts load correctly

--- client-public/src/App.jsx
+++ client-public/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import PostDetail from "./pages/PostPage";
 
 function App() {
   return (
@@ -8,7 +7,7 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/posts/:id" element={<PostDetail />} />
+          <Route path="/posts/:id" element={<PostPage />} />
         </Routes>
       </div>
     </Router>
